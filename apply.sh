1185e2afec7c34ff22dbfd9ed3e56ad3e21888a7
Nathan Bleuzen <nathan.bleuzen-ext@faurecia.com>
2020-06-12 19:32:48 +0200

    Add minicom and screen config files

--- apply.sh
+++ apply.sh
@@ -1,4 +1,6 @@
 ln -fs $(pwd)/.aliases $HOME
 ln -fs $(pwd)/.zshrc $HOME
-ln -fs $(pwd)/.i3 $HOME
 rm -rf $HOME/.oh-my-zsh/custom/ ; ln -fs $(pwd)/.oh-my-zsh/custom/ $HOME/.oh-my-zsh/
+ln -fs $(pwd)/.minirc.dfl $HOME
+ln -fs $(pwd)/.screenrc $HOME
+ln -fs $(pwd)/.i3 $HOME

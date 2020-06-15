f09ca291f242158b97db16ec80072399d85077db
Nathan Bleuzen <nathan.bleuzen-ext@faurecia.com>
2020-06-15 10:45:32 +0200

    Add GNOME terminator config file

--- apply.sh
+++ apply.sh
@@ -3,4 +3,5 @@ ln -fs $(pwd)/.zshrc $HOME
 rm -rf $HOME/.oh-my-zsh/custom/ ; ln -fs $(pwd)/.oh-my-zsh/custom/ $HOME/.oh-my-zsh/
 ln -fs $(pwd)/.minirc.dfl $HOME
 ln -fs $(pwd)/.screenrc $HOME
+ln -fs $(pwd)/.config/terminator/config $HOME/.config/terminator/
 ln -fs $(pwd)/.i3 $HOME
